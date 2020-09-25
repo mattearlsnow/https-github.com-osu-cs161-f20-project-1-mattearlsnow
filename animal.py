743fedea569223e10fca0a2c11cc89a355286833
matt <snowmat@oregonstate.edu>
2020-09-25 14:46:04 -0400

    again

--- animal.py
+++
@@ -1,7 +0,0 @@
-# Author: Matthew Snow
-# Date: 9/19/2020
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
